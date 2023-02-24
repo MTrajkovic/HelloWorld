5ac012f6c05d526687ea37dcad5812f5f20fb177
Milos Trajkovic <mtrajkovic060@gmail.com>
2023-02-24 12:00:58 +0100

    Fix: Candidate Class

--- Typescript/src/app/app.component.ts
+++ Typescript/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Candidate } from './candidate.model';
 import { BaseEntety } from './BaseEntity.model';
 import { Product } from './product.model';
 import { User } from './user.model';
@@ -15,18 +16,7 @@ export class AppComponent implements OnInit {
   title = 'Typescript';
 
   ngOnInit(): void {
-    class Candidate {
-      firstName:string;
-      lastName:string;
-      birthday:Date;
 
-      constructor (firstName:string,lastName:string,birthday:Date){
-        this.firstName=firstName;
-        this.lastName=lastName;
-        this.birthday=birthday;
-      }
-
-    }
       const candidate = new Candidate('Ivan','Ivanovic',new Date(1998 , 26,2))
       const candidate2 = new Candidate('Milan','Milanic',new Date(1977 , 15,7))
       console.log(candidate,candidate2);
---
+++ Typescript/src/app/candidate.model.ts
@@ -0,0 +1,12 @@
+export class Candidate {
+  firstName:string;
+  lastName:string;
+  birthday:Date;
+
+  constructor (firstName:string,lastName:string,birthday:Date){
+    this.firstName=firstName;
+    this.lastName=lastName;
+    this.birthday=birthday;
+  }
+}
+
