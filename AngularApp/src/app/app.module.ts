3cbcdce1a99609125df40777e6f78708178d32c4
Milos Trajkovic <mtrajkovic060@gmail.com>
2023-02-28 13:20:12 +0100

    GoldenBook App - structure

--- AngularApp/src/app/app.module.ts
+++ AngularApp/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { BarsComponent } from './bars/bars.component';
+import { BooksComponent } from './books/books.component';
+import { RowsComponent } from './rows/rows.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    BarsComponent,
+    BooksComponent,
+    RowsComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
