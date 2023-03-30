08d05f322925b338bd5a5edb1fa320668fdc69a2
Milos Trajkovic <mtrajkovic060@gmail.com>
2023-03-30 08:53:22 +0200

    futer and filter

--- AngularApp/src/app/core/components/footer/footer.component.ts
+++ AngularApp/src/app/core/components/footer/footer.component.ts
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
+  Year: number | Date = new Date().getFullYear();
   constructor() { }
 
   ngOnInit(): void {
+    this.Year;
   }
 
 }
